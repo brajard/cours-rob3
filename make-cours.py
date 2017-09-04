8255b258fb07ecb4da9accf7501bad17d74ea580
brajard <julien.brajard@upmc.fr>
2017-09-04 10:23:22 +0200

    planning 2017

---
+++ make-cours.py
@@ -0,0 +1,14 @@
+cours = dict()
+cours[1] = {date:'18 Septembre 2017',
+            titre:'Algorithmes de base',
+            flist:['algorithmes'],
+            output:''}
+
+ic = 1
+
+cur = cours[ic]
+
+#copier header
+# ajouter ce qu'il faut
+# régler paramètres du makefile
+# lancer le makefile
